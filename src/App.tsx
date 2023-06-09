dcd6f00f483e8035d7f406750d055f7798165fec
Albano Futa <99339143+iNOfutta@users.noreply.github.com>
2023-06-09 18:04:38 +0100

    Send all

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const person = ["Dario, ", "21 years old"];
+
+  return (
+    <>
+      <p>Eu estou aqui com: {person}</p>
+    </>
+  );
+}
+
+export default App;
